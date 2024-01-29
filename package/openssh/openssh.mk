80449339a47386d2e0ca01bdf47673477c910127
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2024-01-29 13:29:04 -0500

    openssh: Restore OpenSSL 1.0.2 support
    Fixed some other compilation issues
    
    PROD-5158

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -12,8 +12,6 @@ OPENSSH_CPE_ID_UPDATE = $(OPENSSH_VERSION_MINOR)
 OPENSSH_SITE = http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable
 OPENSSH_LICENSE = BSD-3-Clause, BSD-2-Clause, Public Domain
 OPENSSH_LICENSE_FILES = LICENCE
-
-# 0001-better-detection-of-broken-fzero-call-used-regs.patch
 OPENSSH_AUTORECONF = YES
 
 OPENSSH_CONF_ENV = \
