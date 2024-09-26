be2ba493b6192e7597b40ca1e17cab5a1666cbaa
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-09-25 21:22:40 -0400

    python-daemon: Bump to version 3.0.2

--- package/python-daemon/python-daemon.mk
+++ package/python-daemon/python-daemon.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-PYTHON_DAEMON_VERSION = 3.0.1
-PYTHON_DAEMON_SITE = https://files.pythonhosted.org/packages/84/50/97b81327fccbb70eb99f3c95bd05a0c9d7f13fb3f4cfd975885110d1205a
+PYTHON_DAEMON_VERSION = 3.0.2
+PYTHON_DAEMON_SOURCE = python_daemon-$(PYTHON_DAEMON_VERSION).tar.gz
+PYTHON_DAEMON_SITE = https://files.pythonhosted.org/packages/4a/4a/746aebd8a8f841c53720ef40bbec8f2075d07041db29d01a2a50cd1899e7
 PYTHON_DAEMON_LICENSE = Apache-2.0 (library), GPL-3.0+ (test, build)
 PYTHON_DAEMON_LICENSE_FILES = LICENSE.ASF-2 LICENSE.GPL-3
 PYTHON_DAEMON_SETUP_TYPE = setuptools
