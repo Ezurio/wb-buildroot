e40f6628cfb9acaac1ea0abebba2a142669d7169
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-09-02 12:36:41 -0400

    carbon: Moved ti firmware repo url to github
    PROD-11256
    PROD-27887

--- boot/ti-k3-boot-firmware/ti-k3-boot-firmware.mk
+++ boot/ti-k3-boot-firmware/ti-k3-boot-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TI_K3_BOOT_FIRMWARE_VERSION = 11.01.10
+TI_K3_BOOT_FIRMWARE_VERSION = 11.00.10
 TI_K3_BOOT_FIRMWARE_SITE = $(call github,TexasInstruments,ti-linux-firmware,$(TI_K3_BOOT_FIRMWARE_VERSION))
 TI_K3_BOOT_FIRMWARE_SOURCE = ti-linux-firmware-$(TI_K3_BOOT_FIRMWARE_VERSION).tar.gz
 TI_K3_BOOT_FIRMWARE_INSTALL_IMAGES = YES
