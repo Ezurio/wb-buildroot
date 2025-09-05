c439e82a60ef9255662612e53ef55f0aadede85b
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-09-05 12:28:23 -0400

    Remove host cython dependency for host builds
    PROD-36093

--- package/python-dbus-fast/python-dbus-fast.mk
+++ package/python-dbus-fast/python-dbus-fast.mk
@@ -16,7 +16,6 @@ PYTHON_DBUS_FAST_DEPENDENCIES = \
 	host-python-cython \
 	host-python-setuptools
 HOST_PYTHON_DBUS_FAST_DEPENDENCIES = \
-	host-python-cython \
 	host-python-poetry-core \
 	host-python-setuptools
 
