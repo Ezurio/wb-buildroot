243af296465e3782dfe5fa4d4dfca4af5dd4c4a5
Chris Trowbridge <chris.trowbridge@ezurio.com>
2025-12-18 14:58:44 -0500

    python-dbus-fast: Remove host package
    Refs: PROD-50747

--- package/python-dbus-fast/python-dbus-fast.mk
+++ package/python-dbus-fast/python-dbus-fast.mk
@@ -14,9 +14,5 @@ PYTHON_DBUS_FAST_ENV = REQUIRE_CYTHON=1
 PYTHON_DBUS_FAST_DEPENDENCIES = \
 	host-python-cython \
 	host-python-setuptools
-HOST_PYTHON_DBUS_FAST_DEPENDENCIES = \
-	host-python-poetry-core \
-	host-python-setuptools
 
 $(eval $(python-package))
-$(eval $(host-python-package))
