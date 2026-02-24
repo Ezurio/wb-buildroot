9c4e0d63ea4fba681fceafdb5c8f41eb312f86a6
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2026-02-24 17:37:33 -0500

    imx-gpu: Fiixed permissions on gpu devices and mali installation
    PROD-27490

--- package/freescale-imx/imx-gpu-mali/imx-gpu-mali.mk
+++ package/freescale-imx/imx-gpu-mali/imx-gpu-mali.mk
@@ -11,22 +11,28 @@ IMX_GPU_MALI_SOURCE = mali-imx-$(IMX_GPU_MALI_VERSION)-$(IMX_GPU_MALI_REVISION).
 IMX_GPU_MALI_LICENSE = NXP Semiconductor Software License Agreement
 IMX_GPU_MALI_LICENSE_FILES = EULA COPYING
 IMX_GPU_MALI_PROVIDES = libegl libgles libgbm
+IMX_GPU_MALI_INSTALL_STAGING = YES
 
 define IMX_GPU_MALI_EXTRACT_CMDS
 	$(call NXP_EXTRACT_HELPER,$(IMX_GPU_MALI_DL_DIR)/$(IMX_GPU_MALI_SOURCE))
 endef
 
 define IMX_GPU_MALI_INSTALL_STAGING_CMDS
-	mkdir -p $(STAGING_DIR)/usr/lib $(STAGING_DIR)/usr/include
-	cp -fr $(@D)/usr/lib/*.so* $(STAGING_DIR)/usr/lib
-	cp -fr $(@D)/usr/include/* $(STAGING_DIR)/usr/include
+	mkdir -p $(STAGING_DIR)/usr/include $(STAGING_DIR)/usr/lib
+	cp -fr -t $(STAGING_DIR)/usr/include $(@D)/usr/include/*
+	cp -fr -t $(STAGING_DIR)/usr/lib \
+		$(@D)/usr/lib/*.so* $(@D)/usr/lib/pkgconfig
 endef
 
 define IMX_GPU_MALI_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/lib $(TARGET_DIR)/usr/bin $(TARGET_DIR)/etc
-	cp -fr $(@D)/usr/lib/* $(TARGET_DIR)/usr/lib
-	cp -fr $(@D)/usr/bin/* $(TARGET_DIR)/usr/bin
-	cp -fr $(@D)/etc/* $(TARGET_DIR)/etc
+	mkdir -p $(TARGET_DIR)/etc $(TARGET_DIR)/usr/lib $(TARGET_DIR)/usr/bin
+	cp -fr -t $(TARGET_DIR)/etc $(@D)/etc/*
+	cp -fr -t $(TARGET_DIR)/usr/lib \
+		$(@D)/usr/lib/*.so.* $(@D)/usr/lib/libmali.so $(@D)/usr/lib/firmware
+	cp -fr -t $(TARGET_DIR)/usr/bin $(@D)/usr/bin
+
+	$(INSTALL) -D -m 644 -t $(TARGET_DIR)/usr/lib/udev/rules.d \
+		$(IMX_GPU_MALI_PKGDIR)/51-mali.rules 
 endef
 
 $(eval $(generic-package))
