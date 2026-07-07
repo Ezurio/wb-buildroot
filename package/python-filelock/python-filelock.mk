6b02065fbe0d0228c86eade9bb007d6b5c619e7f
Chris Trowbridge <chris.trowbridge@ezurio.com>
2026-07-07 14:46:30 -0400

    python-click,-crcmod,-filelock,-ruamel-yaml: Add host package
    Refs: PROD-67927

--- package/python-filelock/python-filelock.mk
+++ package/python-filelock/python-filelock.mk
@@ -11,8 +11,10 @@ PYTHON_FILELOCK_SETUP_TYPE = hatch
 PYTHON_FILELOCK_LICENSE = Public Domain
 PYTHON_FILELOCK_LICENSE_FILES = LICENSE
 PYTHON_FILELOCK_DEPENDENCIES = host-python-hatch-vcs
+HOST_PYTHON_FILELOCK_DEPENDENCIES = host-python-hatch-vcs
 
 # 0001-Fix-TOCTOU-symlink-vulnerability-in-lock-file-creation.patch
 PYTHON_FILELOCK_IGNORE_CVES += CVE-2025-68146
 
 $(eval $(python-package))
+$(eval $(host-python-package))
